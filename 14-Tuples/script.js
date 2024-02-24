5ab81f20130e44ae7323c7976cfeb0dce467a68f
Hasnain Raza <hasnainraza3899@gmail.com>
2024-02-24 10:55:28 +0500

    tuples in typescript

---
+++ 14-Tuples/script.js
@@ -0,0 +1,21 @@
+"use strict";
+// In Typescript, tuples are data structure that allows you to store a fixed size collection of elements of different types.They are similar to arrays,but with  a key difference: the types of elements in a tuple are fixed and declared at the time of creation, where arrays can hold elements of the same type, and their size can vary.
+// function to display person information
+const displayPersonInfo = (person) => {
+    const [name, age, isLicience] = person;
+    return `Name: ${name}, Age: ${age},drivingLicience: ${isLicience ? "Yes" : "No"}`;
+};
+// example usage
+const person1 = ["Raza", 24, false];
+// const person2: PersonInfo = [26, true, "Ali"]  // order matter in tuples
+const person2 = ["Ali", 26, true];
+console.log(displayPersonInfo(person1));
+console.log(displayPersonInfo(person2));
+const product1 = ["Laptop", 5000, 3];
+const product2 = ["Watch", 1400, 8];
+const displyProductInfo = (product) => {
+    const [name, price, quantity] = product;
+    return `Name: ${name} , Price: ${price}, Quantity:${quantity} `;
+};
+console.log(displyProductInfo(product1));
+console.log(displyProductInfo(product2));
---
+++ 14-Tuples/script.ts
@@ -0,0 +1,49 @@
+// In Typescript, tuples are data structure that allows you to store a fixed size collection of elements of different types.They are similar to arrays,but with  a key difference: the types of elements in a tuple are fixed and declared at the time of creation, where arrays can hold elements of the same type, and their size can vary.
+
+// Real life example of using tuples in typescript:
+
+// let's consider a scenario where you want to represent a person's basic information,including their name ,age and whether they have a driver's licience.Using a tuple can be an appropriate choice because these three elements have a specific order and type.
+
+
+// define a tuple type for persons information
+type PersonInfo = readonly [string, number, boolean]
+
+
+// function to display person information
+const displayPersonInfo = (person: PersonInfo) => {
+
+    const [name, age, isLicience] = person
+    return `Name: ${name}, Age: ${age},drivingLicience: ${isLicience ? "Yes" : "No"}`
+}
+
+
+// example usage
+const person1: PersonInfo = ["Raza", 24, false]
+// const person2: PersonInfo = [26, true, "Ali"]  // order matter in tuples
+const person2: PersonInfo = ["Ali", 26, true]
+
+console.log(displayPersonInfo(person1))
+console.log(displayPersonInfo(person2))
+
+
+
+//Question 1:
+
+// you are building a simple e-commerce application and need to store product information. Define a tuple type call ProductInfo to represent each product, containing  the following elements.
+
+// name, price quantity 
+// display the information of product by using this tuple type
+
+type ProductInfo = [string, number, number]
+
+const product1: ProductInfo = ["Laptop", 5000, 3]
+const product2: ProductInfo = ["Watch", 1400, 8]
+
+const displyProductInfo = (product: ProductInfo) => {
+    const [name, price, quantity] = product
+    return `Name: ${name} , Price: ${price}, Quantity:${quantity} `
+}
+
+console.log(displyProductInfo(product1))
+console.log(displyProductInfo(product2))
+
