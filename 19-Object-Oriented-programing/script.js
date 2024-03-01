ea60e9baef7a88d6c7f30996f0cc1c53da7b5fb1
Hasnain Raza <hasnainraza3899@gmail.com>
2024-03-01 13:13:16 +0500

    object oriented programing class in typescript

---
+++ 19-Object-Oriented-programing/script.js
@@ -0,0 +1,16 @@
+"use strict";
+// How to create a class and also instance of class in TS
+// A class in terms of OOP  is a blueprint for creating objects
+// a class is like a blueprint for creating similar things.
+// In Typescript, there is a convention to use PascalCase (also known as UpperCamelCase) for class names.This means that class name should start with a capital letter and each subsequent word in the class name should also begin with a capital letter.
+class Person {
+    constructor(name, age, hobbies) {
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies;
+    }
+}
+const person1 = new Person("Raza", 15, ["reading", "playing"]);
+const person2 = new Person("Ali", 13, ["painting", "playing"]);
+console.log(person1);
+console.log(person2);
